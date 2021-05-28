c4001516fb6198d6528d4f4b9cea833930442e4a
hanchaeeun <yaebo98@naver.com>
2021-05-28 21:20:49 +0900

    edited for you page

--- src/Foryou.tsx
+++ src/Foryou.tsx
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+import infoAnimation from './lotties/home_signal.json';
+import './Foryou.css';
+
+interface Language{
+  lang: string;
+  clicked: boolean;
+}
 
 const Foryou = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: infoAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+  const [infoClicked, setInfoClicked] = useState<boolean>(false);
+  const handleInfo = () => {
+    setInfoClicked(!infoClicked);
+  }
+
+  const [languageClicked, setLanguageClicked] = useState<boolean>(false);
+  const handleLang = () => {
+    setLanguageClicked(!languageClicked);
+  }
+
+  const [languagesChecked, setLanguagesChecked] = useState<Language[]>([
+    {lang: 'English', clicked: false},
+    {lang: '한국어', clicked: false},
+    {lang: '简体字', clicked: false},
+    {lang: '繁體字', clicked: false},
+    {lang: '日本語', clicked: false},
+    {lang: 'Others', clicked: false}
+  ]);
+  const handleLangCheck = (language:string) =>{
+    let tmp:Language[] = [];
+    languagesChecked.map((item)=>{
+      if(item.lang !== language){
+        tmp.push(item)
+      }else{
+        tmp.push({lang: item.lang, clicked: !item.clicked})
+        console.log(tmp)
+      }
+    })
+    setLanguagesChecked(tmp);
+  }
   return (
     <div>
-      FORYOU
+      <div className='page-header-container'>
+        <div className='page-header'>
+          For You
+          {infoClicked ? (
+            //info 열려있을 때
+            <div>
+              <button className='information-button' onClick={e => handleInfo()}/>
+              <div className='info-popup'>
+                <div className='info-triangle'/>
+                <div className='info-rectangle'>
+                  <button className='close-button' onClick={e=>handleInfo()}/>
+                  <div className='lottie-container'>
+                    <Lottie 
+                      options={defaultOptions}
+                    />
+                  </div>
+                  <div className='info-text-container'>
+                    <div className='info-title'>For You</div>
+                    <div className='info-content'>We recommend you pages you'll love and need. The more you highlight, the smarter our algorithm gets.</div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          ):(//info 닫혔을 때
+            <button className='information-button' onClick={e => handleInfo()}/>
+          )}
+          <div className='language-container'>
+            {languageClicked ? (
+              //language dropdown 열려있을 때
+              <div>
+                <button className='language-button button' onClick={e => handleLang()}>
+                  Language
+                  <div className='set-language-button close'></div>
+                </button>
+                <div className='language-dropdown'>
+                  <ul className='dropdown'>
+                    <div className='dropdown-header'>Select for feed</div>
+                    {languagesChecked.map((item)=>{
+                      if(item.clicked){
+                        return(
+                          <li className='dropdown-content checked' onClick={() => handleLangCheck(item.lang)}>
+                            {item.lang}
+                            <div className='checked-img'></div>
+                          </li>
+                        )
+                      }else{
+                        return(
+                          <li className='dropdown-content' onClick={() => handleLangCheck(item.lang)}>
+                            {item.lang}
+                          </li>
+                        )}
+                    })}
+                  </ul>
+                </div>
+              </div>
+            ):( //language dropdown 닫혀있을 때
+              <div>
+                <button className='language-button button' onClick={e => handleLang()}>
+                  Language
+                  <div className='set-language-button open'></div>
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className='page-subtitle'>Picked by LINER, just for you</div>
     </div>
   );
 };
--- src/RightMenu.tsx
+++ src/RightMenu.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RightMenu.css';
 
+interface KeywordTag{
+  id: number;
+  tag: string;
+}
+interface TrendPage{
+  id: number;
+  tags?: Tag[];
+  title: string;
+  source: string;
+}
+interface Tag{
+  id: number;
+  tag: string;
+}
+
 const RightMenu = () => {
+  const keywordTags : KeywordTag[] = [
+    { id: 0, tag: '#sharing'},
+    { id: 1, tag: '#free'},
+    { id: 2, tag: '#news'},
+    { id: 3, tag: '#technology'},
+    { id: 4, tag: '#video'},
+    { id: 5, tag: '#education'},
+    { id: 6, tag: '#ncbi'},
+    { id: 7, tag: '#nlm'},
+    { id: 8, tag: '#health'},
+  ]
+  const trendPages: TrendPage[] = [
+    { id: 0, tags: [{id:9, tag:'#david foster wallace'}, {id: 10, tag:'#education'}], title:'How to Remember What You Read - Farnam Street', source:'fs.blog'},
+    { id: 1, tags: [{id:11, tag:'#google'}, {id: 12, tag:'#google search'}, {id: 13, tag:'#list'}, {id: 14, tag:'#tips'}], title:'20 Google Search Tips to Use Google More Efficiently - Lifehack', source:'www.lifehack.org'},
+    { id: 2, title:'Feed | LinkedIn', source:'www.linkedin.com'},
+    { id: 3, title:'Times Tables Rock Stars: Play', source:'play.ttrockstars.com'},
+    { id: 4, title:'ongMaker - Chrome Music Lab', source:'musiclab.chromeexperiments.com'},
+  ]
   return (
     <div className='rightmenu-container'>
       <div className='trending-container'>
@@ -9,16 +42,11 @@ const RightMenu = () => {
         <div className='border'/>
         <div className='trending-content'>
           <div className='keyword-tag-list'>
-            <div className='tag'>#sharing</div>
-            <div className='tag'>#free</div>
-            <div className='tag'>#news</div>
-            <div className='tag'>#technology</div>
-            <div className='tag'>#video</div>
-            <div className='tag'>#education</div>
-            <div className='tag'>#ncbi</div>
-            <div className='tag'>#medline</div>
-            <div className='tag'>#nlm</div>
-            <div className='tag'>#health</div>
+            {keywordTags.map((item)=>{
+              return(
+                <button className='tag'>{item.tag}</button>
+              )
+            })}
           </div>
         </div>
       </div>
@@ -26,41 +54,28 @@ const RightMenu = () => {
         <div className='trending-title'>Trending Pages</div>
         <div className='border'/>
         <div className='trending-content'>
-          <div className='pages-content'>
-            <div className='pages-tag-list'>
-              <div className='tag'>#david foster wallace</div>
-              <div className='tag'>#education</div>
-            </div>
-            <div className='title'>How to Remember What You Read - Farnam Street</div>
-            <div className='source'>fs.blog</div>
-          </div>
-
-          <div className='pages-content'>
-            <div className='pages-tag-list'>
-              <div className='tag'>#google</div>
-              <div className='tag'>#google search</div>
-              <div className='tag'>#list</div>
-              <div className='tag'>#tips</div>
-            </div>
-            <div className='title'>20 Google Search Tips to Use Google More Efficiently - ...</div>
-            <div className='source'>www.lifehack.org</div>
-          </div>
-
-          <div className='pages-content'>
-            <div className='title'>Feed | LinkedIn</div>
-            <div className='source'>www.linkedin.com</div>
-          </div>
-
-          <div className='pages-content'>
-            <div className='title'>Times Tables Rock Stars: Play</div>
-            <div className='source'>play.ttrockstars.com</div>
-          </div>
-
-          <div className='pages-content'>
-            <div className='title'>SongMaker - Chrome Music Lab</div>
-            <div className='source'>musiclab.chromeexperiments.com</div>
-          </div>
-
+          {trendPages.map((item)=>{
+            console.log(item.tags)
+            if(item.tags !== undefined){
+              return(
+              <div className='pages-content'>
+                <div className='pages-tag-list'>
+                  {item.tags?.map((t)=>{
+                    return(
+                      <div className='tag'>{t.tag}</div>
+                  )})}
+                </div>
+                <div className='title'>{item.title}</div>
+                <div className='source'>{item.source}</div>
+              </div>
+            )} else{
+              return(
+                <div className='pages-content'>
+                  <div className='title'>{item.title}</div>
+                  <div className='source'>{item.source}</div>
+                </div>
+              )}
+          })}
           <div className='show-more'>
             Show More
             <img src='https://getliner.com/src/images/expand.svg'/>
