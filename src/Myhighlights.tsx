344260979ae4d7373ecd6ee8374b707267d09965
hanchaeeun <yaebo98@naver.com>
2021-05-30 00:50:04 +0900

    edit my highlights

--- src/Myhighlights.tsx
+++ src/Myhighlights.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import Lottie from 'react-lottie';
 import infoAnimation from './lotties/myhighlights.json';
+import './Myhighlights.css';
+
+interface MyContent{
+  title: string;
+  highlight: string[];
+  url: string;
+  tags?: string[];
+  img?: string;
+  sourceImg: string;
+}
 
 const Myhighlights = () => {
   const defaultOptions = {
@@ -12,9 +22,22 @@ const Myhighlights = () => {
   const handleInfo = () => {
     setInfoClicked(!infoClicked);
   }
+  const mainContents: MyContent[] =[
+    {
+      title: "CSS: 선택자(Selector) 이해",
+      highlight: ['한 페이지 내에서 여러 번 반복될 필요가 있는 스타일은 클래스 선택자를 사용하고, 단 한번 유일하게 적용될 스타일은 ID선택자를 사용하는 것이 좋습니다','ID선택자의 우선순위가 클래스 선택자의 우선순위보다 높습니다.'],
+      url: 'https://www.nextree.co.kr/p8468/',
+      sourceImg:'https://getliner.com/src/images/favicon.svg',
+      tags: ['css']
+    },
+  ]
+  let highlightSum: number = 0;
+  mainContents.map((item)=>{//calculate the num of my highlights
+    highlightSum += item.highlight.length;
+  })
   return (
     <div className='page-content'>
-      <div className='page-header-container'>
+      <div className='myhighlight-header-container'>
         <div className='sticky-header'>
           <div className='page-header'>
             My Highlights
@@ -43,6 +66,24 @@ const Myhighlights = () => {
               <button className='information-button' onClick={e => handleInfo()}/>
             )}
           </div>
+        </div>
+        <div className='summary'>
+          <div className='pages'>
+            {mainContents.length} Pages
+          </div>
+          <div className='middleDot'/>
+          <div className='highlights'>
+            {highlightSum} Highlights
+          </div>
+        </div>
+      </div>
+      <div className='myhighlights-buttons-container'>
+        <div className='tab-container'>
+          <button className='highlight-button'>Highlights</button>
+          <button className='tags-button'>Tags</button>
+        </div>
+        <div className='filter-container'>
+
         </div>
       </div>
     </div>
